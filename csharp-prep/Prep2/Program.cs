cdef8d8c90c097dc30dbb18f8b78123b9057ba8f
elabanca <elabanca@byui.edu>
2024-04-27 15:55:24 -0600

    prep 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int gradePer = int.Parse(userInput);
+
+        string letter  = "";
+
+        if (gradePer >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePer >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePer >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePer >= 60)
+        {
+            letter= "D";
+        }
+        else 
+        {
+            letter= "F";
+        }
+
+        Console.WriteLine($"Your Garde is {letter}");
+        
+        if (gradePer >= 70)
+            {
+                Console.WriteLine("Congratulations!");
+            }
+            else
+                {
+                    Console.WriteLine("It was not this time. Do your best and try again");
+                }
     }
 }
