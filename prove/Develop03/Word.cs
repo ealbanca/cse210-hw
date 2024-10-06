7fa5e1596a2c91d0bd22ddcaaf5e073ff8eea8f1
elabanca <elabanca@byui.edu>
2024-10-05 22:29:54 -0600

    Added classes and attributes

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,34 @@
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+
+public class Word
+{
+    private string _text;
+    public bool _isHidden;
+
+    public Word(string text)
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+
+    }
+
+}
