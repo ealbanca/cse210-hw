2892e67e7147fd06667a9ad2cce26a7977e5c204
elabanca <elabanca@byui.edu>
2024-10-11 22:32:28 -0600

    updated program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Reference ref1 = new Reference("Moroni", 10, 5, 6);
-        Console.WriteLine(ref1.GetDisplayText());
+        Scripture scr1 = new Scripture(ref1, "5 And by the power of the Holy Ghost ye may know the truth of all things.\n" +
+        "6 And whatsoever thing is good is just and true; wherefore, nothing that is good denieth the Christ, but acknowledgeth that he is.");
+
+
+        Console.WriteLine(scr1.GetDisplayText());
+        //Console.WriteLine(ref1.GetDisplayText());
+
+        Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+
+        while (scr1.IsCompletelyHidden())
+        {
+
+
+
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "quit")
+            {
+                break;
+            }
+            else if (input == "")
+            {
+                scr1.HideRandomWords(1);
+            }
+        }
+
+        if (scr1.IsCompletelyHidden())
+        {
+            Console.Clear();
+        }
 
         /*Reference ref2 = new Reference("Moroni", 10, 1, 5);
         Console.WriteLine(ref2.GetDisplayVerses());*/
