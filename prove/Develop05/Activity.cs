376a4f6997c99acaa2fbba2ba8edeb85228cfdeb
elabanca <elabanca@byui.edu>
2024-10-23 21:10:11 -0600

    Updated Activity

--- prove/Develop05/Activity.cs
+++ prove/Develop05/Activity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 namespace Develop05
 {
@@ -18,21 +19,43 @@ namespace Develop05
         }
         public void DisplayStartingMessage()
         {
+            Console.WriteLine($"Welcome to the {_name}.");
+            Console.WriteLine($"{_description}");
+            ShowCountDown(3);
 
         }
         public void DisplayEndingMessage()
         {
-
+            Console.WriteLine("Well Done!!");
+            Console.WriteLine($"You have completed another{_duration}seconds of the {_name}");
+            ShowSpinner(3);
         }
 
         public void ShowSpinner(int seconds)
         {
-
+            for (int i = 0; i < seconds; i++)
+            {
+                Console.Write("|");
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+                Console.Write("/");
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+                Console.Write("—");
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+            }
         }
 
         public void ShowCountDown(int seconds)
         {
-
+            for (int i = seconds; i > 0; i--)
+            {
+                Console.Write($"{i} ");
+                Thread.Sleep(1000);
+                Console.Write("\b\b");
+            }
+            Console.WriteLine(" ");
         }
     }
 }
