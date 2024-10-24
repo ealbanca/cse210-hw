13116bea7b2cff0161693d058c70a74eb21085fb
elabanca <elabanca@byui.edu>
2024-10-23 22:07:39 -0600

    Updated Reflecting prompts and questions

--- prove/Develop05/ReflectingActivity.cs
+++ prove/Develop05/ReflectingActivity.cs
@@ -7,18 +7,22 @@ public class ReflectingActivity : Activity
 
     private List<string> _prompts = new List<string>
     {
-        ""
+        "Think of a time when you felt the love of our Savior",
+        "Think of a time when the scriptures have stregthened your testimony ",
+        "Think of a time when you visited the temple and learned something new",
+        "Think of a time when you felt the Holy Ghost"
     };
 
     private List<string> _questions = new List<string>
     {
-
+        "What did you feel after that experience?",
+        "What did you learn from that experience?",
+        "How did that experince changed your life?",
+        "Have you had any other experience like that?"
     };
 
-    public ReflectingActivity(string name, string description, int duration, List<string> _prompts, List<string> _questions) : base(name, description, duration)
+    public ReflectingActivity(string name, string description, int duration) : base(name, description, duration)
     {
-        _prompts = _prompts;
-        _questions = _questions;
     }
 
     public void Run()
