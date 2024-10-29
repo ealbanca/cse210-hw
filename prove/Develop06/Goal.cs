ba4f361912b431ac9a51c23f99fdb87a21dbde48
elabanca <elabanca@byui.edu>
2024-10-28 19:10:24 -0600

    Updated Goal Class

--- prove/Develop06/Goal.cs
+++ prove/Develop06/Goal.cs
@@ -1,13 +1,13 @@
 using System;
 
-public class Goal
+public abstract class Goal
 {
 
     private string _shortName;
     private string _description;
-    private string _points;
+    private int _points;
 
-    public Goal(string shortName, string description, string points)
+    protected Goal(string shortName, string description, int points)
     {
 
         _shortName = shortName;
@@ -15,23 +15,42 @@ public class Goal
         _points = points;
     }
 
-    public void RecordEvent()
+    public string GetShortName()
     {
-
+        return _shortName;
     }
 
-    public bool IsComplete()
+    public void SetShortName(string shortName)
     {
-
+        _shortName = shortName;
     }
 
-    public string GetDetailsString()
+    public string GetDescription()
     {
-
+        return _description;
     }
 
-    public string GetStringRepresentation()
+    public void SetDescription(string description)
     {
-
+        _description = description;
     }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+    public void SetPoints(int points)
+    {
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public abstract string GetDetailsString();
+
+    public abstract string GetStringRepresentation();
+
 }
